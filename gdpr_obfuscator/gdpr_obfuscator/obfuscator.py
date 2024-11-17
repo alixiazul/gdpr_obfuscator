3dc6edd37a5118f3d9120d9bf4f7d62fb2f38282
alixiazul <alicia.rodriguez.t@gmail.com>
2024-11-17 18:58:35 +0000

    Changes spaces to black formatter

--- gdpr_obfuscator/gdpr_obfuscator/obfuscator.py
+++ gdpr_obfuscator/gdpr_obfuscator/obfuscator.py
@@ -1,13 +1,16 @@
 import json
 import os
 import boto3
+import csv
+import io
 from urllib.parse import urlparse
 from botocore.exceptions import ClientError
 
+
 class Obfuscator:
-    """
-    """
-    def __init__(self, json_string:str):
+    """ """
+
+    def __init__(self, json_string: str):
         """
         Initializes an instance of the class by parsing the provided JSON string.
 
@@ -16,8 +19,8 @@ class Obfuscator:
         is raised. The extracted information is stored in instance variables.
 
         Arguments:
-            json_string (str): A JSON-formatted string containing the file location 
-            and optional fields to obfuscate. The string should include the key 
+            json_string (str): A JSON-formatted string containing the file location
+            and optional fields to obfuscate. The string should include the key
             'file_to_obfuscate' and optionally 'pii_fields'.
 
         Raises:
@@ -25,7 +28,7 @@ class Obfuscator:
             KeyError: If the 'file_to_obfuscate' key is missing in the JSON string.
             ValueError: If the 'file_to_obfuscate' key is empty in the JSON string.
         """
-        
+
         # If there the json string is empty then a Value error is raised
         if not json_string:
             raise ValueError("JSON string cannot be empty")
@@ -36,23 +39,23 @@ class Obfuscator:
         self.file_to_obfuscate = file_location
         self.pii_fields = fields
 
-    def __get_data(self, json_string:str):
+    def __get_data(self, json_string: str):
         """
         Parses a JSON string to extract the file location and optional fields to obfuscate.
 
-        This function loads a JSON string, checks for the presence of required keys, 
-        and returns the file path and a list of fields to obfuscate. If the necessary 
+        This function loads a JSON string, checks for the presence of required keys,
+        and returns the file path and a list of fields to obfuscate. If the necessary
         keys are missing or invalid, appropriate exceptions are raised.
 
         Arguments:
-            json_string (str): A JSON-formatted string containing the file path 
-            and optional fields to obfuscate. The string must include the key 
+            json_string (str): A JSON-formatted string containing the file path
+            and optional fields to obfuscate. The string must include the key
             'file_to_obfuscate' and optionally 'pii_fields'.
 
         Returns:
             tuple: A tuple containing:
                 - file_name (str): The value associated with the 'file_to_obfuscate' key.
-                - pii_fields (list): A list of fields to obfuscate, defaults to an empty list 
+                - pii_fields (list): A list of fields to obfuscate, defaults to an empty list
                 if the key 'pii_fields' is missing.
 
         Raises:
@@ -65,43 +68,40 @@ class Obfuscator:
             data = json.loads(json_string)
 
         except json.JSONDecodeError as e:
-            
+
             # Raise the error with the element that causes the error and the position
-            raise json.JSONDecodeError("Invalid JSON string provided", json_string, e.pos) from e
-        
+            raise json.JSONDecodeError(
+                "Invalid JSON string provided", json_string, e.pos
+            ) from e
+
         # If the key "file_to_obfuscate" is not present then raise a Key Error
         if "file_to_obfuscate" not in data:
             raise KeyError("Key 'file_to_obfuscate' is missing in the JSON string")
-        
+
         # Get the value of the key
         file_name = data.get("file_to_obfuscate", "")
 
         # If empty, raise a value error
         if not file_name:
             raise ValueError("The field 'file_to_obfuscate' cannot be empty")
-        
-        
+
         pii_fields = data.get("pii_fields", [])
         return file_name, pii_fields
-
 
     @property
     def pii_fields(self):
         return self.__pii_fields
-    
 
     @pii_fields.setter
-    def pii_fields(self, value:list):
+    def pii_fields(self, value: list):
         self.__pii_fields = value
-
 
     @property
     def file_to_obfuscate(self):
         return self.__file_to_obfuscate
 
-
     @file_to_obfuscate.setter
-    def file_to_obfuscate(self, file_name:str):
+    def file_to_obfuscate(self, file_name: str):
         """
         Sets the file to be obfuscated by validating its existence and checking if it is a valid file in an S3 bucket.
 
@@ -113,7 +113,7 @@ class Obfuscator:
 
         Raises:
             ValueError: If the file is not valid, does not exist, or is not found in the S3 bucket.
-        
+
         Side Effects:
             If the file is valid, the provided file name is stored in the `__file_to_obfuscate` attribute for further processing.
 
@@ -124,8 +124,9 @@ class Obfuscator:
 
         # If not a valid file, raise an error
         if not self.__is_valid_file(file_name):
-            raise ValueError(f"The file '{file_name}' does not exist or is not a valid file")
-        
+            raise ValueError(
+                f"The file '{file_name}' does not exist or is not a valid file"
+            )
 
         # Check if the file exists in the data bucket
         s3 = boto3.client("s3")
@@ -136,37 +137,37 @@ class Obfuscator:
             s3.head_object(Bucket=bucket_name, Key=key)
         except ClientError as e:
             # Raise an error if the file does not exist
-            raise ValueError(f"File '{file_name}' not found in S3 bucket '{bucket_name}'")
+            raise ValueError(
+                f"File '{file_name}' not found in S3 bucket '{bucket_name}'"
+            )
 
         # Set the attribute
         self.__file_to_obfuscate = file_name
-
 
     def __is_valid_file(self, file_path: str) -> bool:
         """
         Determines if the given file path is valid.
 
-        This function checks the validity of the file path by determining if it 
+        This function checks the validity of the file path by determining if it
         points to a local file or a file stored in an S3 bucket. If the path starts
-        with "s3://", it is treated as an S3 file path and validated accordingly. 
+        with "s3://", it is treated as an S3 file path and validated accordingly.
         Otherwise, it checks the file's existence as a local file.
 
         Arguments:
-            file_path (str): The path to the file to validate. It can be either a 
+            file_path (str): The path to the file to validate. It can be either a
             local file path or an S3 file path.
 
         Returns:
-            bool: True if the file path points to an existing local file or a valid 
+            bool: True if the file path points to an existing local file or a valid
             S3 key. False otherwise.
         """
 
-        if file_path.startswith('s3://'):
+        if file_path.startswith("s3://"):
             # Check if it's an S3 URL
             return self.__is_valid_s3_file(file_path)
         else:
             # Check if it's a local file
             return os.path.isfile(file_path)
-        
 
     def __is_valid_s3_file(self, s3_url: str) -> bool:
         """
@@ -178,27 +179,26 @@ class Obfuscator:
         Returns:
             bool: True if the file exists and is accessible in the specified S3 bucket, otherwise False.
         """
-        s3 = boto3.client('s3')
+        s3 = boto3.client("s3")
         parsed_url = urlparse(s3_url)
         bucket_name = parsed_url.netloc
-        key = parsed_url.path.lstrip('/')
+        key = parsed_url.path.lstrip("/")
 
         try:
             s3.head_object(Bucket=bucket_name, Key=key)
             return True
         except s3.exceptions.ClientError:
             return False
-        
 
     def __parse_s3_uri(self, s3_uri: str):
         """
         Parses an S3 URI and extracts the bucket name and key.
 
-        This function validates the given S3 URI to ensure it starts with "s3://". 
+        This function validates the given S3 URI to ensure it starts with "s3://".
         After validation, it splits the URI into the bucket name and the key.
 
         Arguments:
-            s3_uri (str): The S3 URI to parse. It must be in the format 
+            s3_uri (str): The S3 URI to parse. It must be in the format
             "s3://bucket_name/key".
 
         Returns:
@@ -211,7 +211,41 @@ class Obfuscator:
         """
         if not s3_uri.startswith("s3://"):
             raise ValueError("Invalid S3 URI")
-        
+
         s3_uri = s3_uri[5:]
         bucket_name, key = s3_uri.split("/", 1)
         return bucket_name, key
+
+    def obfuscate(self) -> io.StringIO:
+        """
+        Obfuscates the PII fields in a CSV file.
+        Each PII field will be replaced with '***'.
+
+        Returns:
+            A byte-stream representation of the obfuscated file.
+        """
+
+        # Create a byte stream
+        byte_stream = io.StringIO()
+
+        with open(self.file_to_obfuscate, "r") as file:
+            reader = csv.DictReader(file)
+            rows = list(reader)
+            fieldnames = reader.fieldnames
+
+        # Process the rows and obfuscate PII fields
+        for row in rows:
+            for pii_field in self.pii_fields:
+                if pii_field in row:
+                    row[pii_field] = "***"
+
+        # Writing the obfuscated data to the byte-stream
+        writer = csv.DictWriter(byte_stream, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(rows)
+
+        # Reset the pointer to the beginning of the byte stream
+        byte_stream.seek(0)
+
+        # Convert the byte stream to an UTF-8 encoded byte representation
+        return byte_stream.getvalue().encode("utf-8")
--- gdpr_obfuscator/test/test_obfuscator.py
+++ gdpr_obfuscator/test/test_obfuscator.py
@@ -21,16 +21,18 @@ def s3_client(aws_credentials):
         yield boto3.client("s3", region_name="eu-west-2")
 
 
-def json_string_with_valid_s3_file(s3_client, bucket_name:str, file_name:str, pii_fields:list=None):
+def json_string_with_valid_s3_file(
+    s3_client, bucket_name: str, file_name: str, pii_fields: list = None
+):
     # Mock S3 bucket and object with explicit region
     s3_client.create_bucket(
         Bucket=bucket_name,
-        CreateBucketConfiguration={'LocationConstraint': "eu-west-2"}
+        CreateBucketConfiguration={"LocationConstraint": "eu-west-2"},
     )
 
     # Mock a valid file to the bucket
     s3_client.put_object(Bucket=bucket_name, Key=file_name, Body="test data")
-  
+
     # If pii_fields are defined then use them in the json_string
     pii_fields_str = f'"pii_fields": {json.dumps(pii_fields)}' if pii_fields else ""
 
@@ -47,7 +49,9 @@ class TestObfuscator:
 
     def test_obfuscator_throws_an_error_with_an_empty_json_script(self):
         json_string = "{}"
-        with pytest.raises(KeyError, match="Key 'file_to_obfuscate' is missing in the JSON string"):
+        with pytest.raises(
+            KeyError, match="Key 'file_to_obfuscate' is missing in the JSON string"
+        ):
             Obfuscator(json_string)
 
     def test_obfuscator_throws_an_error_with_a_json_script_malformed(self):
@@ -55,28 +59,38 @@ class TestObfuscator:
         with pytest.raises(json.JSONDecodeError):
             Obfuscator(json_string)
 
-    def test_obfuscator_throws_an_error_with_a_json_script_with_only_one_correct_key_but_no_value(self):
+    def test_obfuscator_throws_an_error_with_a_json_script_with_only_one_correct_key_but_no_value(
+        self,
+    ):
         json_string = """{
             "file_to_obfuscate"
         }"""
         with pytest.raises(json.JSONDecodeError):
             Obfuscator(json_string)
 
-    def test_obfuscator_throws_an_error_with_a_json_script_with_only_one_correct_key_but_malformed(self):
+    def test_obfuscator_throws_an_error_with_a_json_script_with_only_one_correct_key_but_malformed(
+        self,
+    ):
         json_string = """{
             "file_to_obfuscate":
         }"""
         with pytest.raises(json.JSONDecodeError):
             Obfuscator(json_string)
 
-    def test_obfuscator_throws_an_error_with_a_json_script_with_only_one_correct_key_and_empty_value(self):
+    def test_obfuscator_throws_an_error_with_a_json_script_with_only_one_correct_key_and_empty_value(
+        self,
+    ):
         json_string = """{
             "file_to_obfuscate":""
         }"""
-        with pytest.raises(ValueError, match="The field 'file_to_obfuscate' cannot be empty"):
+        with pytest.raises(
+            ValueError, match="The field 'file_to_obfuscate' cannot be empty"
+        ):
             Obfuscator(json_string)
 
-    def test_obfuscator_throws_an_error_with_a_json_script_with_invalid_file_to_obfuscate(self):
+    def test_obfuscator_throws_an_error_with_a_json_script_with_invalid_file_to_obfuscate(
+        self,
+    ):
         json_string = """{
             "file_to_obfuscate":"asdf"
         }"""
@@ -84,8 +98,9 @@ class TestObfuscator:
         with pytest.raises(ValueError):
             Obfuscator(json_string)
 
-
-    def test_obfuscator_throws_an_error_with_a_json_script_with_invalid_s3_file(self, s3_client):
+    def test_obfuscator_throws_an_error_with_a_json_script_with_invalid_s3_file(
+        self, s3_client
+    ):
 
         bucket_name = "my-ingestion-bucket"
         file_name = "nonexistent-file.csv"
@@ -93,7 +108,7 @@ class TestObfuscator:
         # Mock S3 bucket and object with explicit region
         s3_client.create_bucket(
             Bucket=bucket_name,
-            CreateBucketConfiguration={'LocationConstraint': "eu-west-2"}
+            CreateBucketConfiguration={"LocationConstraint": "eu-west-2"},
         )
 
         # Mock a non existing file
@@ -102,9 +117,32 @@ class TestObfuscator:
         expected_message = f"The file 's3://{bucket_name}/{file_name}' does not exist or is not a valid file"
         with pytest.raises(ValueError, match=expected_message):
             Obfuscator(json_string)
-    
 
-    def test_obfuscator_ok_json_script_with_valid_s3_file_no_pii_fields(self, s3_client):
+    def test_obfuscator_throws_an_error_with_a_json_script_with_file_not_in_s3_format(
+        self, s3_client
+    ):
+
+        bucket_name = "my-ingestion-bucket"
+        file_name = "nonexistent-file.csv"
+
+        # Mock S3 bucket and object with explicit region
+        s3_client.create_bucket(
+            Bucket=bucket_name,
+            CreateBucketConfiguration={"LocationConstraint": "eu-west-2"},
+        )
+
+        # Mock a non existing file
+        json_string = f'{{"file_to_obfuscate": "{file_name}"}}'
+
+        expected_message = (
+            f"The file '{file_name}' does not exist or is not a valid file"
+        )
+        with pytest.raises(ValueError, match=expected_message):
+            Obfuscator(json_string)
+
+    def test_obfuscator_ok_json_script_with_valid_s3_file_no_pii_fields(
+        self, s3_client
+    ):
         bucket_name = "my-ingestion-bucket"
         file_name = "existent-file.csv"
 
@@ -113,12 +151,26 @@ class TestObfuscator:
         assert obfuscator.file_to_obfuscate == f"s3://{bucket_name}/{file_name}"
         assert obfuscator.pii_fields == []
 
-
-    def test_obfuscator_ok_json_script_with_valid_s3_file_and_pii_fields(self, s3_client):
+    def test_obfuscator_ok_json_script_with_valid_s3_file_and_pii_fields(
+        self, s3_client
+    ):
         bucket_name = "my-ingestion-bucket"
         file_name = "existent-file.csv"
         pii_fields = ["name", "email_address"]
 
-        json_string = json_string_with_valid_s3_file(s3_client, bucket_name, file_name, pii_fields)
+        json_string = json_string_with_valid_s3_file(
+            s3_client, bucket_name, file_name, pii_fields
+        )
         obfuscator = Obfuscator(json_string)
         assert obfuscator.pii_fields == pii_fields
+
+    def test_obfuscator_obfuscate_pii_fields(self, s3_client):
+        bucket_name = "my-ingestion-bucket"
+        file_name = "new_data/file1.csv"
+        pii_fields = ["name", "email_address"]
+
+        json_string = json_string_with_valid_s3_file(
+            s3_client, bucket_name, file_name, pii_fields
+        )
+        obfuscator = Obfuscator(json_string)
+        assert obfuscator.obfuscate() == True
