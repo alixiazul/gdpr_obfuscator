63e4b307817d87177a6fbdea81fcb24449678cf3
alixiazul <alicia.rodriguez.t@gmail.com>
2024-12-22 01:36:13 +0000

    Add a logger and change the blueprint for a lambda function that uses the obfuscator.

--- gdpr_obfuscator/lambda_obfuscator.py
+++ gdpr_obfuscator/lambda_obfuscator.py
@@ -1,27 +1,53 @@
 import json
 import boto3
+import logging
 from obfuscator import Obfuscator
 
 def lambda_handler(event, context):
-    try:
+
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+    pii_fields = []
+
+    if "Records" in event:
         bucket_name = event['Records'][0]['s3']['bucket']['name']
         file_key = event['Records'][0]['s3']['object']['key']
+        file_to_obfuscate = f"s3://{bucket_name}/{file_key}"
+        pii_fields = ["name", "email_address"]
+    else:
+        file_to_obfuscate = event.get('file_to_obfuscate')
+        pii_fields = event.get('pii_fields')
+        bucket_name, file_key = file_to_obfuscate.replace("s3://", "").split("/", 1)
 
-        s3 = boto3.client('s3')     
-   
-        json_string = f'{{"file_to_obfuscate": "s3://{bucket_name}/{file_key}"}}'
-        obfuscator = Obfuscator(json_string)
-        obfuscated_file = obfuscator.obfuscate()
-        
-        response = s3.put_object(Bucket=bucket_name, Key=file_key, Body=obfuscated_file.getvalue().encode('utf-8'))
-        
+    if not file_to_obfuscate:
         return {
-            'statusCode': 200,
-            'body': response
+            'statusCode': 500,
+            'body': 'file_to_obfuscate is missing'
         }
     
+    json_string = json.dumps({"file_to_obfuscate": file_to_obfuscate, "pii_fields": pii_fields})
+    obfuscator = Obfuscator(json_string)
+
+    try:
+        obfuscated_file = obfuscator.obfuscate()
+    except ValueError as ve:
+        return {
+            'statusCode': 400,
+            'body': json.dumps(f"ValueError: {str(ve)}")
+        }
     except Exception as e:
         return {
             'statusCode': 500,
             'body': json.dumps(f"Error: {str(e)}")
         }
+
+    s3 = boto3.client('s3')
+    s3.put_object(Bucket=bucket_name, Key=file_key, Body=obfuscated_file.getvalue().encode('utf-8'))    
+    logger.info(f"Processing file: {file_to_obfuscate}")
+
+    return {
+        'statusCode': 200,
+        'body': 'File obfuscated successfully'
+    }
+
